c6c7bf9844edb35bbdd096cc00410c1210686007
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2024-01-20 11:23:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/meals', async (req, res) => {
+app.get('/', async (req, res) => {
   const meals = await fs.readFile('./data.json', 'utf8');
   res.json(JSON.parse(meals));
 });
